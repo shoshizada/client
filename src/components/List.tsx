2014b41fbef2610da69f095503a95946f64694a4
chaniYehudit <p0548453160@gmail.com>
2022-08-04 18:45:03 +0300

    op to git

---
+++ src/components/List.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+interface User{  
+    firstName: string
+      lastName: string
+    }
+
+const  List=() =>{
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        getAllUsers();
+    }, [])
+
+    const getAllUsers = async () => {
+        const res = await axios.get<User[]>(`http://localhost:3333/user/getAll`);
+        console.log(res);
+        setUsers(res.data);
+    }
+    const renderList = () : JSX.Element[]=>
+    {
+        return users.map((user)=>{
+            return(<>
+                <p>{user.firstName}</p>
+            <p>{user.lastName} </p></>
+        )      
+        })}
+        return (
+            <>
+           <div>{renderList()}</div>
+            </>
+        )
+    
+}
+export default List;
+
