448848c7596f7d53959735d12926f15bc702294f
shoshizada <s0556737338@gmail.comgit clone https://github.com/shoshizada/WeightProject.git>
2022-08-30 14:11:58 +0300

    getbyid

--- src/components/System/GetBusinessById.tsx
+++ src/components/System/GetBusinessById.tsx
@@ -1,112 +1,53 @@
-import React, { useCallback, useMemo, useRef, useState } from "react";
-import {
-  GoogleMap,
-  Marker,
-  DirectionsRenderer,
-  Circle,
-  MarkerClusterer,
-  useLoadScript,
-  MarkerClustererProps
-} from '@react-google-maps/api'
-import Place from './Place'
-import { position } from "@chakra-ui/react";
-import Distance from './distance';
-type LatLngLiteral = google.maps.LatLngLiteral;
-type DirectionsResult = google.maps.DirectionsResult;
-type MapOptions = google.maps.MapOptions;
-const Map = () => {
-  const [office,setOffice]=useState<LatLngLiteral|any>()
-  const [directions,setDirections]=useState<DirectionsResult|any>();
-  const mapRef = useRef<GoogleMap>();
-  const center = useMemo<LatLngLiteral>(() => ({ lat: 31.79995377521336, lng: 35.22120734478211 }), []);
-  const onLoad=useCallback((map:any)=>(mapRef.current=map),[]);
-  const house=useMemo(()=>g(center),[center])
-  const [zoom,setZoom]=useState(9);
-  const image = "https://static.thenounproject.com/png/331569-200.png";
-  const fetchDirections=(house:LatLngLiteral)=>
-  {
-    if(!office) return;
-    const service=new google.maps.DirectionsService();
-    service.route(
-      {
-        origin:house,
-        destination:office,
-        travelMode:google.maps.TravelMode.DRIVING
-      },
-      (result,status)=>
-      {
-        if(status==='OK' &&result){
-          setDirections(result)
+import React from 'react';
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import { getAllSystems, getSystemByManagerId } from '../../api/system';
+    interface System{
+        _id: string
+        topic: string
+        objectName: string
+        owner: string
+        description: string
+        phone: string
+        email: string
+        urlName:string
         }
-      }
-    )
-  }
-  // const center={lat:40,lng:-80};
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyC3OCX35jO7Q0qKZn89UfSQUBYXMpTdl3E",
-    libraries: ["places"],
-  })
-  if (!isLoaded) return <div>Loading...</div>;
-  return (
-    <div className="search-place-container">
-      <div className="controls">
-        <Place setOffice={(position)=>{
-          setOffice(position);
-          mapRef.current?.panTo(position);
-          setZoom(14);
-        }}/>
-        {!office}
-        {/* {!directions && <Distance leg={directions.routes[0].legs[0]}/>} */}
-      </div>
-      <div className="map">
-        <GoogleMap zoom={zoom} center={center} mapContainerClassName={"map-container"}onLoad={onLoad} >
-          <Marker position={office}></Marker>
-        {directions && <DirectionsRenderer directions={directions} options={{ polylineOptions:{
-          zIndex:50,
-          strokeColor:"#1976D2",
-          strokeWeight:5
-        }}}/>}
-          {office &&(
-             <>
-             <Marker position={office} icon={{url:image,scaledSize:new google.maps.Size(73,70)}} />
-             <MarkerClusterer>
-              {(clusterer:any|MarkerClustererProps | Readonly<MarkerClustererProps>): any=>
-               house.map((h) => (
-              <Marker
-               key={h.lat}
-                position={h}
-                clustered={clusterer}
-                onClick={()=>{
-                  fetchDirections(h)
-                }}
-                />
-                ))
-                }
-             </MarkerClusterer>
-          <Circle center={office} radius={1500}
-             options={{fillColor:'green',fillOpacity:0.1,strokeColor:'green'}}/>
-          <Circle center={office} radius={3000}
-             options={{fillColor:'orange',fillOpacity:0.1,strokeColor:'orange'}}/>
-          <Circle center={office} radius={4500}
-             options={{fillColor:'red',fillOpacity:0.1,strokeColor:'red'}}/>
-          </>
-          )}
-        </GoogleMap>
-      </div>
-    </div>
-  )
-}
-;
-const g= (position:LatLngLiteral)=>
+const GetSystemById=()=>
 {
-  const _h: Array<LatLngLiteral>=[];
-  for(let i=0;i<8;i++)
-  {
-    const d=Math.random() < 0.5 ?-2 : 2;
-    _h.push({
-      lat:position.lat+Math.random()/d,
-      lng:position.lng+Math.random()/d,
-    });
-  }
-  return _h;
+      const navigate = useNavigate();
+         const [idGetBusiness, setIdGetBusiness] = useState("");
+    const [system,setSystem] = useState<System>();
+    const getSystem = async ()=> {
+         const res =getSystemByManagerId(idGetBusiness);
+        console.log(res);
+        setSystem(await res);
+        }
+   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+       navigate('/updatesystem');
+      };
+   const handleClick1 = (event: React.MouseEvent<HTMLElement>) => {
+       navigate('/deletesystem');
+      };
+        return (
+            <>
+            <h1>GetBusinessById</h1>
+             <Input onChange={(e) => setIdGetBusiness(e.target.value)} type='string' placeholder='ID OF BUSINESS TO GET'></Input>
+             <button onClick={getSystem} >get system by id</button>
+             {system!=undefined?  <div><>
+                <p>id: {system._id} </p>
+                <p>topic: {system.topic}</p>
+                <p>objectName: {system.objectName} </p>
+                <p>owner: {system.owner}</p>
+                <p>description: {system.description} </p>
+                <p>phone: {system.phone} </p>
+                <p>email: {system.email}</p>
+                <p>urlName: {system.urlName} </p>
+                </></div>:<div></div>}
+                <button onClick={handleClick}>update system</button>
+                <button onClick={handleClick1}>delete system</button>
+            </>
+        )
 }
+export default GetSystemById;
